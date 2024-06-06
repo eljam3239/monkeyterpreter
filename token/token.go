355ec7ffba72cec44b3c2fa6de5aee113cd32f48
Eli <elijames2301@gmail.com>
2024-06-05 21:49:32 -0400

    Write token type constants

--- token/token.go
+++ token/token.go
@@ -6,3 +6,32 @@ type Token struct {
   Type TokenType
   Literal string
 }
+
+const (
+
+  ILLEGAL = "ILLEGAL"
+  EOF     = "EOF"
+
+  //identifiers & literals
+  IDENT = "IDENT"
+  INT   = "INT"
+
+  //Operators
+  ASSIGN = "="
+  PLUS   = "+"
+
+  //Delimiters
+  COMMA     = ","
+  SEMICOLON = ";"
+
+  LPAREN    = "("
+  RPAREN    = ")"
+  LBRACE    = "{"
+  RBRACE    = "}"
+
+  //Keywords
+  FUNCTION = "FUNCTION"
+  LET      = "LET"
+  
+
+)
