0a8856e5fe16835197738dd80d1d197d30daa22f
Eli <elijames2301@gmail.com>
2024-06-09 23:14:16 -0400

    Error handling in lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,3 +25,12 @@ func TestNextToken(t *testing.T) {
 }
 
 l := New(input)
+
+for i, tt := range tests {
+	tok := l.NextToken()
+
+	if tok.Type != tt.expectedType {
+		t.Fatalf("tests[%d] - tokentype wrong, expected=%q, got=%q"
+			i, tt.expectedType, tokType)
+	}
+}
