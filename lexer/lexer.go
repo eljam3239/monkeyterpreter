565cfabf12c0b0c87ca55f5957c48300ddd50965
Eli <elijames2301@gmail.com>
2024-06-10 23:09:37 -0400

    Write lexer.go

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,42 +1,13 @@
-//source code -> tokens
 package lexer
 
-import (
-	"testing"
-
-	"monkeyterpreter/token"
-)
-
-func TestNextToken(t *testing.T) {
-	input := '=+(){},;'
-	tests := []struct {
-		expectedType	token.TokenType
-		expectedLiteral	string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
+type Lexer struct {
+	input		 string
+	position	 int //cur pos in input
+	readPosition int //cur reading position in input	
+	ch 			 byte //current char 
 }
 
-l := New(input)
-
-for i, tt := range tests {
-	tok := l.NextToken()
-
-	if tok.Type != tt.expectedType {
-		t.Fatalf("tests[%d] - tokentype wrong, expected=%q, got=%q"
-			i, tt.expectedType, tokType)
-	}
-
-	if tok.Literal != tt.expectedLiteral {
-		t.Fatalf("tests[%d] - literal wrong, expected=%q, got=%q",
-			i.tt.expectedLiteral, tok.Literal)
-	}
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
 }
