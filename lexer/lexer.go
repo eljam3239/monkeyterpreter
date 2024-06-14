4810eeaa1662022858ba46e695442404f35132db
Eli <elijames2301@gmail.com>
2024-06-13 22:58:40 -0400

    Write cases

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,3 +31,21 @@ func New(input string) *Lexer {
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
+
+import "monkey/token"
+func (l *Lexer) NextToken() token.Token {
+var tok token.Token
+switch l.ch {
+case '=':
+tok = newToken(token.ASSIGN, l.ch)
+case ';':
+tok = newToken(token.SEMICOLON, l.ch)
+case '(':
+tok = newToken(token.LPAREN, l.ch)
+case ')':
+tok = newToken(token.RPAREN, l.ch)
+case ',':
+tok = newToken(token.COMMA, l.ch)
+case '+':
+tok = newToken(token.PLUS, l.ch)
+case '{':
